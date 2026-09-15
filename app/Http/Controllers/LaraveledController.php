41555e0554a5b91b4f20954816202da46e47e8aa
J-S-P-K <juasantipeko@gmail.com>
2026-09-15 11:45:57 -0300

    login hecho

---
+++ app/Http/Controllers/LaraveledController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorelaraveledRequest;
+use App\Http\Requests\UpdatelaraveledRequest;
+use App\Models\laraveled;
+
+class LaraveledController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorelaraveledRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(laraveled $laraveled)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(laraveled $laraveled)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatelaraveledRequest $request, laraveled $laraveled)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(laraveled $laraveled)
+    {
+        //
+    }
+}
---
+++ database/factories/LaraveledFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\laraveled;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<laraveled>
+ */
+class LaraveledFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
